2838b190cab6fec7329f23682066fc9e020ca1a2
ShivaPandey02 <shiva.pandey@crowdbotics.com>
2023-04-21 02:00:52 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -2,7 +2,7 @@ export const globalOptions = {
   "name": "epic-39799",
   "url": "https://epic-39799.botics.co",
   "api": "https://epic-39799.botics.co/api/v1",
-  "initialRoute": "Untitled2"
+  "initialRoute": "Untitled3"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
