597cf5b13aaa3b65b642c40c8ce6c75a86c331ff
crowdbotics-apps <team@crowdbotics.com>
2023-05-16 15:54:25 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Player(models.Model):
     'Generated Model'
     name = models.TextField()
+    level = models.IntegerField(null=True,blank=True,)
 class Character(models.Model):
     'Generated Model'
     name = models.TextField()
