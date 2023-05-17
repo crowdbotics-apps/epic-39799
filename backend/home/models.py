73e18b434d117ca925f3b3ee0137ad9eb0389e1c
crowdbotics-apps <team@crowdbotics.com>
2023-05-17 13:20:57 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -7,3 +7,12 @@ class Character(models.Model):
 class Player(models.Model):
     'Generated Model'
     name = models.TextField()
+    username = models.CharField(max_length=256,null=True,blank=True,)
+    email = models.EmailField(max_length=254,null=True,blank=True,)
+    friends = models.ManyToManyField("home.Player",blank=True,related_name="player_friends",)
+    profilePicture = models.URLField(null=True,blank=True,)
+    rank = models.IntegerField(null=True,blank=True,)
+    level = models.IntegerField(null=True,blank=True,)
+    acceptedTerms = models.BooleanField(null=True,blank=True,)
+    acceptedPrivacyPolicy = models.BooleanField(null=True,blank=True,)
+    isBlocked = models.BooleanField(null=True,blank=True,)
